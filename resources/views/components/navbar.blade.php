8d2940c1781bf6e645ee38da0d9715921b8fd005
Abid Mafahim <abidmafahim@upi.edu>
2024-02-05 08:29:08 +0700

    Initial commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,6 @@
+<div clas="nav-container">
+    <div class="nav-buttons">
+        aaa
+        <x-forms.button type="primary" label="Lihat Lowongan" size="small" />
+    </div>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/home.css', 'resources/css/navbar.css', 'resources/js/home.js'])
+    <title>BKK SMKN 1 Karawang</title>
+</head>
+<body>
+    <x-navbar />
+</body>
+</html>
