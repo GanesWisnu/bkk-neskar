fd1f49450bc3416d8a1790970852f4107dcddcb2
bagussr <bagusssyamsu@gmail.com>
2024-03-08 20:33:23 +0700

    provide user API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,20 +10,72 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      */
+
     public function index()
+    {
+        return view('admin.user.index');
+    }
+
+    public function create()
+    {
+        return view('admin.user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' =>'required|email',
+            'password' =>'required',
+        ]);
+
+        $request = $request->except(['token_csrf']);
+
+        $user = User::create($request);
+
+        if ($user->save()){
+            return redirect()->route('admin.user.index');
+        }
+    }
+
+    public function edit(int $id)
+    {
+        $user = User::find($id);
+
+        return view('admin.user.edit', ['user' => $user]);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $user = User::find($id);
+
+        $request = $request->except(['csrf_token']);
+
+        if($user->update($request)){
+            return redirect->route('admin.user.index');
+        }
+    }
+
+    public function delete(int $id)
+    {
+        $user = User::find($id);
+        if ($user->delete()){
+            return redirect->route('admin.user.index');
+        }
+    }
+
+    public function index_login()
     {
         return view('pages.login');
     }
 
 
-
     /**
-     * Store a newly created resource in storage.
+     * login  resource in storage.
      */
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required',
+            'email' => 'required|email',
             'password' => 'required',
         ]);
 
