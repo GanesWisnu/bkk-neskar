5a4b32c98e85fd95241484a0d98f0121b1492952
bagussr <bagusssyamsu@gmail.com>
2024-03-18 21:17:09 +0700

    migrations

---
+++ database/migrations/2024_03_17_150912_alter_table_job_vacancies_id.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE `job_vacancies` ADD `code` VARCHAR NULL;');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_vacancies', function (Blueprint $table) {
+            //
+            DB::statement('ALTER TABLE `job_vacancies` DROP `code`;');
+        });
+    }
+};
