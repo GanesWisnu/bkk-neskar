54ed402572404993453ffb26deaf0fc2cf449db9
Abid Mafahim <abidmafahim@upi.edu>
2024-03-25 15:57:33 +0700

    Update style on components and lowongan detail page

--- resources/views/layouts/user/home/perusahaan.blade.php
+++ resources/views/layouts/user/home/perusahaan.blade.php
@@ -4,15 +4,8 @@
         <p class="perusahaan-description">Kami bermitra dengan perusahaan-perusahaan yang berperan dalam memajukan karier lulusan kami, menciptakan peluang pertumbuhan profesional yang menjanjikan.</p>
     </div>
     <div class="perusahaan-mitra">
-        <img src="{{ asset('images/logos/Honda.png') }}" alt="Honda" class="mitra-item">
-        <img src="{{ asset('images/logos/Ajinomoto.png') }}" alt="Ajinomoto" class="mitra-item">
-        <img src="{{ asset('images/logos/Mitsubishi.png') }}" alt="Mitsubishi" class="mitra-item">
-        <img src="{{ asset('images/logos/Suzuki.png') }}" alt="Suzuki" class="mitra-item">
-        <img src="{{ asset('images/logos/Hino.png') }}" alt="Hino" class="mitra-item">
-        <img src="{{ asset('images/logos/Kalbe_Farma.svg') }}" alt="Kalbe Farma" class="mitra-item">
-        <img src="{{ asset('images/logos/Indofood_CBP.svg') }}" alt="Indofood CBP" class="mitra-item">
-        <img src="{{ asset('images/logos/Indomaret.png') }}" alt="Indomaret" class="mitra-item">
-        <img src="{{ asset('images/logos/JIAEC.png') }}" alt="JIAEC" class="mitra-item">
-        <img src="{{ asset('images/logos/Gramedia.png') }}" alt="Gramedia" class="mitra-item">
+        @foreach ($companies as $item => $company)
+            <img src="{{ asset('images/upload/' . $company->image) }}" alt="{{ $company->name }}" class="mitra-item" />
+        @endforeach
     </div>
 </section>
